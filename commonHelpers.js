a6c61adb59a60975943ba570a1602f86dfa78738
a-nester <73670421+a-nester@users.noreply.github.com>
2024-03-29 13:45:04 +0000

    Deploying to gh-pages from @ a-nester/goit-js-hw-10@6b8701eb46e7ad698c4722a3345c9a2f72fcc09f 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as t}from"./assets/vendor-2b44ac2e.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};console.log("Hi");const n=document.querySelector("#datetime-picker");t(n,o);
+//# sourceMappingURL=commonHelpers.js.map
